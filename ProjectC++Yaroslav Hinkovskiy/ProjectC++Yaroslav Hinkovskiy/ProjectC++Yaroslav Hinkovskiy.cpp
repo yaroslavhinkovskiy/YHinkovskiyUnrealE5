1e04e2049669816657d09e1e216a4ffdb5cc7f5f
yaroslavhinkovskiy <miuakkaynt@gmail.com>
2025-12-14 22:59:50 +0200

    Name, Health, Class
    Реалізовано зчитування та зберігання всіх необхідних даних

---
+++ ProjectC++Yaroslav Hinkovskiy/ProjectC++Yaroslav Hinkovskiy/ProjectC++Yaroslav Hinkovskiy.cpp
@@ -0,0 +1,22 @@
+﻿// ProjectC++Yaroslav Hinkovskiy.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string name = "Bill";
+    int health = 100;
+    string characterClass = "magician";
+
+    cout << "Name: " << name << endl;
+    cout << "Health: " << health << endl;
+    cout << "Class: " << characterClass << endl;
+
+    return 0;
+}
+
+
